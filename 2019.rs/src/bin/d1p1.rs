3db75bf1ce02c9a60ffb46550a424e10055dc8b1
Dmitry K <dimka.rs@gmail.com>
2019-12-22 12:20:42 +0300

    2019.rs: day 1, parts 1 and 2

--- 2019.rs/src/bin/d1p1.rs
+++ 2019.rs/src/bin/d1p1.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let f = File::open("./d1p1.in")?;
+    let f = BufReader::new(f);
+
+    let mut sum: i32 = 0;
+
+    for line in f.lines() {
+        sum += calc(line.unwrap().parse().unwrap());
+    }
+
+    println!("Result is {}", sum);
+    Ok(())
+}
+
+fn calc(mass: i32) -> i32 {
+    mass / 3 - 2
 }
--- 2019.rs/src/bin/d1p2.rs
+++ 2019.rs/src/bin/d1p2.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let f = File::open("./d1p1.in")?;
+    let f = BufReader::new(f);
+
+    let mut sum: i32 = 0;
+
+    for line in f.lines() {
+        let mut add_mass = line.unwrap().parse().unwrap();
+        while let Some(new_mass) = calc(add_mass)  {
+            sum += new_mass;
+            add_mass = new_mass;
+        }
+    }
+
+    println!("Result is {}", sum);
+    Ok(())
+}
+
+fn calc(mass: i32) -> Option<i32> {
+    let new_mass = mass / 3 - 2;
+    if new_mass > 0
+    {
+        Some(new_mass)
+    } else {
+        None
+    }
 }
